cfb9000caf9c07dcd3b7e37dda16a11cc32cfdff
Djordje Lukic <djordje.lukic@docker.com>
2024-04-02 23:52:25 +0200

    Temporary partial fix for neovim rendering

--- src/turm.rs
+++ src/turm.rs
@@ -1,8 +1,4 @@
-use crate::{
-    ansi::{AnsiOutput, GraphicRendition},
-    cell::Style,
-    grid::Grid,
-};
+use crate::{ansi::GraphicRendition, cell::Style, grid::Grid};
 
 #[derive(Debug, Default)]
 pub struct CursorPos {
@@ -37,18 +33,18 @@ impl Turm {
 
     pub fn input(&mut self, c: u8) {
         if c == b'\n' {
-            self.move_cursor(0, self.cursor.pos.y + 1);
+            self.move_cursor(self.cursor.pos.x, self.cursor.pos.y + 1);
         } else if c == b'\r' {
             self.move_cursor(0, self.cursor.pos.y);
+            return;
         } else {
+            if self.cursor.pos.x + 1 > self.columns {
+                self.move_cursor(0, self.cursor.pos.y + 1);
+            }
             self.grid[self.cursor.pos.y][self.cursor.pos.x].c = c as char;
             self.grid[self.cursor.pos.y][self.cursor.pos.x].style = self.current_style;
 
             self.move_cursor(self.cursor.pos.x + 1, self.cursor.pos.y);
-        }
-
-        if self.cursor.pos.x == self.columns {
-            self.move_cursor(0, self.cursor.pos.y + 1);
         }
 
         if self.cursor.pos.y == self.lines {
@@ -64,6 +60,13 @@ impl Turm {
     }
 
     pub fn color(&mut self, c: GraphicRendition) {
+        if self.cursor.pos.x == self.columns {
+            self.move_cursor(0, self.cursor.pos.y + 1);
+        }
+        if self.cursor.pos.y == self.lines {
+            self.move_cursor(0, self.lines - 1);
+            self.scroll_up();
+        }
         match c {
             GraphicRendition::ForegroundColor(c) => {
                 self.grid[self.cursor.pos.y][self.cursor.pos.x].style.fg = c;
